b20b58a7d34e4186d038ea0f716b4b73389402f0
Anna Panteleeva <apanteleeva94@gmail.com>
2023-06-01 16:37:59 +0300

    refactor: добвить prettier и устранить конфликт с линтером

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { createRoot } from "react-dom/client";
-import Header from "./components/Header";
+import { createRoot } from 'react-dom/client';
+import Header from './components/Header';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<Header />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build"),
-    },
-    port: 3000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/, // styles files
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        loader: "url-loader",
-        options: { limit: false },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"],
-  },
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public', 'index.html'),
+		}),
+		new MiniCssExtractPlugin(),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'build'),
+		},
+		port: 3000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+			{
+				test: /\.css$/, // styles files
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+				loader: 'url-loader',
+				options: { limit: false },
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx'],
+	},
 };
