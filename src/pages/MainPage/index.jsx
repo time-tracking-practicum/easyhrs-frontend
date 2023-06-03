aaa70a957448f73d501d95f7575708f89d6a69ea
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-06-03 20:43:54 +0400

    added: Добавлена страница MainPage

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,3 +1,12 @@
 import './MainPage.css';
 
-export default function MainPage() {}
+export default function MainPage() {
+	return (
+		<div className="main page__main">
+			<div className="main__menu">
+				<h3 className="main__menu-title">EasyHrs</h3>
+			</div>
+			<div className="main__container" />
+		</div>
+	);
+}
