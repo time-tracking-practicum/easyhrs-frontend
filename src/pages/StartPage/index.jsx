190ea650751b9f01bb7c2679bb41ffab2ecb6613
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-06-06 02:18:37 +0400

    update: Обвновил начальную страницу, добавил градиент и анимацию

--- src/pages/StartPage/index.jsx
+++ src/pages/StartPage/index.jsx
@@ -6,13 +6,22 @@ import startLogo from '../../images/startLogo.png';
 export default function StartPage() {
 	return (
 		<div className="start page__start">
-			<div className="start__container">
-				<img
-					className="start__image"
-					src={startLogo}
-					alt="Логотип приложения EasyHrs"
-				/>
-				<h1 className="start__title">EasyHrs</h1>
+			<div className="start__container-gradient">
+				<div className="start__gradien-top" />
+				<div className="start__gradient-right" />
+				<div className="start__gradient-bottom" />
+			</div>
+			<div className="start__block">
+				<div className="start page__start">
+					<div className="start__container">
+						<img
+							className="start__image"
+							src={startLogo}
+							alt="Логотип приложения EasyHrs"
+						/>
+						<h1 className="start__title">EasyHrs</h1>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
