69f6ff59075b34188340563ea4153337767649a9
Anna Panteleeva <apanteleeva94@gmail.com>
2023-06-06 15:52:54 +0300

    feat: добавить разметку taskcard

--- src/pages/TaskCardPage/index.jsx
+++ src/pages/TaskCardPage/index.jsx
@@ -1,6 +1,15 @@
 import './TaskCardPage.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function TaskCardPage() {
-	return <section className="taskcardpage">TaskCard</section>;
+	return (
+		<section className="taskcardpage">
+			<div className="taskcardpage__title-wrapper">
+				<Link to="/" className="taskcardpage__crumb" />
+				<h2 className="taskcardpage__title">Задача</h2>
+			</div>
+			<form className="taskcardpage__form">Форма</form>
+		</section>
+	);
 }
