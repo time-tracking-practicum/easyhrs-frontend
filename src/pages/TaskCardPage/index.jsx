37569d38ad005dde7f87a7b5e46d16df55c25805
Anna Panteleeva <apanteleeva94@gmail.com>
2023-06-07 16:00:30 +0300

    feat: добавить текстовые инпуты в taskcard

--- src/pages/TaskCardPage/index.jsx
+++ src/pages/TaskCardPage/index.jsx
@@ -1,15 +1,48 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import './TaskCardPage.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function TaskCardPage() {
+	const [task, setTask] = useState('Дизайн главной страницы'); // стейт значения названия задачи
+	const [project, setProject] = useState('Дачи за городом'); // стейт значения проекта
+
 	return (
 		<section className="taskcardpage">
 			<div className="taskcardpage__title-wrapper">
 				<Link to="/" className="taskcardpage__crumb" />
 				<h2 className="taskcardpage__title">Задача</h2>
 			</div>
-			<form className="taskcardpage__form">Форма</form>
+			<form className="taskcardpage__form">
+				<label className="taskcardpage__label" htmlFor="task">
+					Название
+					<input
+						id="task"
+						className="taskcardpage__input-text"
+						value={task}
+						onChange={(e) => setTask(e.target.value)}
+					/>
+					<div className="taskcardpage__input-icon" />
+				</label>
+				<label className="taskcardpage__label" htmlFor="project">
+					Проект
+					<input
+						id="project"
+						className="taskcardpage__input-text"
+						value={project}
+						onChange={(e) => setProject(e.target.value)}
+					/>
+					<div className="taskcardpage__input-icon" />
+				</label>
+				<div className="taskcardpage__calendar-container">
+					<label className="taskcardpage__label" htmlFor="project">
+						Дата задачи
+					</label>
+					<label className="taskcardpage__label" htmlFor="project">
+						Дедлайн
+					</label>
+				</div>
+			</form>
 		</section>
 	);
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './Router.css';
 
-import StartPage from '../pages/StartPage';
+// import StartPage from '../pages/StartPage';
 import MainPage from '../pages/MainPage';
 import MatrixPage from '../pages/MatrixPage';
 import TaskCardPage from '../pages/TaskCardPage';
@@ -13,7 +13,7 @@ export default function Router() {
 		// компонент необходим для защиты страниц с авторизацией
 		<div className="page">
 			<Routes>
-				<Route path="/" element={<StartPage />} />
+				{/* <Route path="/" element={<StartPage />} /> */}
 				<Route path="/main" element={<MainPage />} />
 				<Route path="/matrix" element={<MatrixPage />} />
 				<Route path="/task-card" element={<TaskCardPage />} />
