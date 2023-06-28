5eb3791f12b1da509908c7932cd05bbe32978bdc
Dmitry Baranov <baranow.dmitry@gmail.com>
2023-06-28 19:51:55 +0300

    feat(sort tasks): sort tasks by click on name button

--- src/components/TasksHeader/index.jsx
+++ src/components/TasksHeader/index.jsx
@@ -1,32 +1,66 @@
 import './TasksHeader.css';
+import { useEffect, useState } from 'react';
 
-const TasksHeader = () => (
-	<div className="task tasksHeader">
-		<div className="task__container tasksHeader__container">
-			<p className="task__name-container tasksHeader__name-container">
-				Название
-				<svg
-					width="8"
-					height="15"
-					viewBox="0 0 14 7"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
+const TasksHeader = ({ sortTasksByName }) => {
+	const [state, setState] = useState(null);
+	const handleClick = () => {
+		setState(!state);
+	};
+
+	useEffect(() => {
+		if (state === null) return;
+		sortTasksByName(state);
+	}, [state]);
+
+	return (
+		<div className="task tasksHeader">
+			<div className="task__container tasksHeader__container">
+				<button
+					className="task__name-container tasksHeader__name-container"
+					onClick={handleClick}
 				>
-					<path
-						fillRule="evenodd"
-						clipRule="evenodd"
-						d="M0 0L7 7L14 0H0Z"
-						fill="#000"
-					/>
-				</svg>
-			</p>
-			<span className="task__project">Проект</span>
-			<span className="task__matrix-wrapper">Приоритет</span>
-			<span className="task__deadline">Дедлайн</span>
-			<span className="task__timer-wrapper">Статус</span>
+					Название
+					{state ? (
+						<svg
+							width="8"
+							height="15"
+							viewBox="0 0 14 7"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fillRule="evenodd"
+								clipRule="evenodd"
+								d="M0 0L7 7L14 0H0Z"
+								fill="#000"
+							/>
+						</svg>
+					) : (
+						<svg
+							width="8"
+							height="15"
+							transform="rotate(180)"
+							viewBox="0 0 14 7"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fillRule="evenodd"
+								clipRule="evenodd"
+								d="M0 0L7 7L14 0H0Z"
+								fill="#000"
+							/>
+						</svg>
+					)}
+				</button>
+				<span className="task__project">Проект</span>
+				<span className="task__matrix-wrapper">Приоритет</span>
+				<span className="task__deadline">Дедлайн</span>
+				<span className="task__timer-wrapper">Статус</span>
+			</div>
+			<span className="tasksHeader__timer">Таймер</span>
 		</div>
-		<span className="tasksHeader__timer">Таймер</span>
-	</div>
-);
+	);
+};
 
 export default TasksHeader;
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,11 +1,22 @@
 import './MainPage.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import Task from '../../components/Task';
 import TasksHeader from '../../components/TasksHeader';
 import Timer from '../../components/Timer';
 
 export default function MainPage({ tasks }) {
+	const [tasksList, setTasksList] = useState(tasks);
+
+	const sortTasksByName = (flag) => {
+		const newTasksList = tasksList.sort((a, b) =>
+			flag ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+		);
+		setTasksList([...newTasksList]);
+	};
+
+	useEffect(() => setTasksList(tasks), [tasks]);
+
 	const [playTimer, setPlayTimer] = useState(false);
 	const [dropTimer, setDropTimer] = useState(false);
 	const [pause, setPause] = useState(false);
@@ -14,9 +25,9 @@ export default function MainPage({ tasks }) {
 		<section className="main">
 			<div className="main__animation main__animation_active">
 				<Header sectionName="Мои задачи" newtask />
-				<TasksHeader />
+				<TasksHeader sortTasksByName={sortTasksByName} />
 				<ul className="main__tasks">
-					{tasks.map((task) => (
+					{tasksList.map((task) => (
 						<Task
 							key={task.id}
 							name={task.name}
