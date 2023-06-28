746ab697f01239d8918e11ded86824680ab68a09
Dmitry Baranov <baranow.dmitry@gmail.com>
2023-06-28 17:56:55 +0300

    feat(tasksheader component): the top row with the names of the columns in the task list

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -2,26 +2,30 @@ import './MainPage.css';
 import React from 'react';
 import Header from '../../components/Header';
 import Task from '../../components/Task';
+import TasksHeader from '../../components/TasksHeader';
 
 export default function MainPage({ tasks }) {
 	return (
 		<section className="main">
-			<Header sectionName="Мои задачи" newtask />
-			<ul className="main__tasks">
-				{tasks.map((task) => (
-					<Task
-						key={task.id}
-						name={task.name}
-						project={task.project}
-						deadline={task.deadline}
-						status={task.status}
-						emoji={task.emoji}
-						urgent={task.is_urgent}
-						important={task.is_important}
-						task={task}
-					/>
-				))}
-			</ul>
+			<div className="main__animation main__animation_active">
+				<Header sectionName="Мои задачи" newtask />
+				<TasksHeader />
+				<ul className="main__tasks">
+					{tasks.map((task) => (
+						<Task
+							key={task.id}
+							name={task.name}
+							project={task.project}
+							deadline={task.deadline}
+							status={task.status}
+							emoji={task.emoji}
+							urgent={task.is_urgent}
+							important={task.is_important}
+							task={task}
+						/>
+					))}
+				</ul>
+			</div>
 		</section>
 	);
 }
