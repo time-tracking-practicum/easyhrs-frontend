887507c22ea87a319658b85e29e312cdb9844cca
Artem <artem.v.mitin@yandex.ru>
2023-06-29 18:41:24 +0300

    feat: add userApi

--- src/components/ProfileAvatarSection/index.jsx
+++ src/components/ProfileAvatarSection/index.jsx
@@ -1,22 +1,75 @@
+/* eslint-disable no-console */
+import { useContext, useRef } from 'react';
 import './ProfileAvatarSection.css';
 import defaultImg from '../../images/avatar.svg';
 import buttonImg from '../../images/icon-pencil_violet.svg';
+import { UserContext } from '../../contexts/UserContext';
+import userApi from '../../utils/UserApi';
 
-export default function ProfileAvatarSection({ image, text }) {
+export default function ProfileAvatarSection({ onSetCurrentUser }) {
+	const { photo, id, email } = useContext(UserContext);
+	const filePicker = useRef(null);
+
+	const handlePick = () => {
+		filePicker.current.click();
+	};
+
+	function encryptPhoto(inputPhoto) {
+		return new Promise((res, rej) => {
+			const reader = new FileReader();
+			reader.readAsDataURL(inputPhoto);
+			reader.onloadend = () => {
+				const { result } = reader;
+				res(result);
+			};
+			reader.onerror = () => {
+				rej(new Error(`Ошибка обработки фото ${inputPhoto}`));
+			};
+		});
+	}
+
+	const handleChange = async (e) => {
+		try {
+			const result = await encryptPhoto(e.target.files[0]);
+			const newUserData = userApi.changeUserData(
+				{
+					email: `${email}`,
+					photo: result.replace(`data:image/jpeg;base64,`, ``),
+				},
+				id
+			);
+			onSetCurrentUser(newUserData);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className="profile__avatar-container">
 			<img
-				src={image ?? defaultImg}
+				src={photo === null ? defaultImg : photo}
 				alt="Аватар пользователя"
 				className="profile__avatar-image"
 			/>
-			<button className="profile__avatar-button" type="button">
+			<input
+				type="file"
+				name="photo"
+				className="profile__avatar-input_hidden"
+				ref={filePicker}
+				onChange={handleChange}
+				accept="image/*,.png,.jpg"
+			/>
+			<button
+				className="profile__avatar-button"
+				type="button"
+				onClick={handlePick}
+			>
 				<img
-					className="profile__avatar-button-img"
-					alt="Картинка кнопки"
 					src={buttonImg}
+					alt="Иконка кнопки"
+					className="profile__avatar-button-img"
 				/>
-				{text}
+				Изменить фото
 			</button>
 		</div>
 	);
---
+++ src/utils/UserApi.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-underscore-dangle */
+class UserApi {
+	constructor(options) {
+		this._url = options.url;
+	}
+
+	static _checkResult = (res) => {
+		if (res.ok) {
+			return res.json();
+		}
+		return Promise.reject(new Error(`Ошибка: ${res.status}`));
+	};
+
+	async getCurrentUser() {
+		const token =
+			localStorage.getItem('token') || sessionStorage.getItem('token');
+		const res = await fetch(`${this._url}/users/me/`, {
+			method: 'GET',
+			headers: {
+				'Content-type': 'application/json',
+				authorization: `Token ${token}`,
+			},
+		});
+		return UserApi._checkResult(res);
+	}
+
+	async changeUserData(data, userId) {
+		const token =
+			localStorage.getItem('token') || sessionStorage.getItem('token');
+		const res = await fetch(`${this._url}/users/${userId}/`, {
+			method: 'PATCH',
+			headers: {
+				'Content-type': 'application/json',
+				authorization: `Token ${token}`,
+			},
+			body: JSON.stringify(data),
+		});
+		return UserApi._checkResult(res);
+	}
+
+	async changeUserPassword(data) {
+		const token =
+			localStorage.getItem('token') || sessionStorage.getItem('token');
+		const res = await fetch(`${this._url}/users/set_password/`, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+				authorization: `Token ${token}`,
+			},
+			body: JSON.stringify(data),
+		});
+		return res;
+	}
+}
+
+const userApi = new UserApi({
+	url: 'https://easyhrs.hopto.org/api/v1',
+});
+
+export default userApi;
