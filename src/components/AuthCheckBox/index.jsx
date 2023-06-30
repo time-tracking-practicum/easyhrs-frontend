ea7415fcced7acae505fe353f5ab68de25e6aff1
Anna Panteleeva <apanteleeva94@gmail.com>
2023-06-30 13:53:26 +0300

    style: Действия с радиокнопкой осуществляются строго при нажатии на окружность

--- src/components/AuthCheckBox/index.jsx
+++ src/components/AuthCheckBox/index.jsx
@@ -1,23 +1,24 @@
 import './AuthCheckBox.css';
 
 export default function AuthCheckBox({ isCheckboxChecked, toggleCheckBox }) {
-	console.log(isCheckboxChecked);
 	return (
-		<label className="authForm__checkbox-container" htmlFor="hello">
-			<input
-				type="checkbox"
-				id="hello"
-				className="authForm__checkbox"
-				checked={isCheckboxChecked}
-			/>
-			<span
+		<div className="authForm__checkbox-container">
+			<label
 				className={`authForm__checkbox-label ${
 					isCheckboxChecked ? 'authForm__checkbox-label_checked' : ''
 				}`}
 				aria-hidden
-				onClick={toggleCheckBox}
-			/>
-			Запомнить меня
-		</label>
+				htmlFor="hello"
+			>
+				<input
+					type="checkbox"
+					id="hello"
+					className="authForm__checkbox"
+					checked={isCheckboxChecked}
+					onChange={toggleCheckBox}
+				/>
+			</label>
+			<p className="authForm__checkbox-text">Запомнить меня</p>
+		</div>
 	);
 }
