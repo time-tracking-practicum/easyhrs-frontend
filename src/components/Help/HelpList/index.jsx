6d08c2bf35adbb664f691511674b33bcf020c3c3
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-07-09 15:27:58 +0400

    added: helpToggle component

--- src/components/Help/HelpList/index.jsx
+++ src/components/Help/HelpList/index.jsx
@@ -8,13 +8,25 @@ function HelpList() {
 		priorityTask: 'Как добавить приоритет для задачи?',
 	};
 
+	const text = {
+		addTask: `
+			Задать дедлайн для задачи можно непосредственно при
+			ее создании выбрав нужную дату в поле “Дедлайн”.
+			Если задача уже создана, то для того, чтобы
+			добавить срок выполнения нужно зайти в параметры задачи
+			и ввести нужную дату в поле “Дедлайн”.
+		`,
+		nothing: 'Никак',
+		plug: 'Может ты подумаешь сам?',
+	};
+
 	return (
 		<div className="help__list">
-			<HelpItem infoName={name.addTask} />
-			<HelpItem infoName={name.addTask} />
-			<HelpItem infoName={name.priorityTask} />
-			<HelpItem infoName={name.addTask} />
-			<HelpItem infoName={name.addTask} />
+			<HelpItem infoName={name.addTask} infoText={text.nothing} />
+			<HelpItem infoName={name.addTask} infoText={text.addTask} />
+			<HelpItem infoName={name.priorityTask} infoText={text.nothing} />
+			<HelpItem infoName={name.addTask} infoText={text.nothing} />
+			<HelpItem infoName={name.addTask} infoText={text.plug} />
 		</div>
 	);
 }
