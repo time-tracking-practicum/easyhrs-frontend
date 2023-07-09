eb96f3e35efa946ee8be2be66df4fb7ee1b4c247
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-07-09 18:43:11 +0400

    added: styles feedback form

--- src/components/Help/HelpForm/index.jsx
+++ src/components/Help/HelpForm/index.jsx
@@ -1,7 +1,27 @@
 import './HelpForm.css';
 
 function HelpForm() {
-	return <>123</>;
+	return (
+		<form className="help__form">
+			<h3 className="help__form-question">Остались вопросы ?</h3>
+			<div className="help__input-container">
+				<input
+					className="help__input"
+					type="email"
+					placeholder="E-mail для связи"
+					required
+				/>
+				<span className="help__feedback">На этот e-mail мы пришлем ответ</span>
+			</div>
+			<textarea
+				className="help__textarea"
+				type="text"
+				placeholder="Описание проблемы"
+				required
+			/>
+			<button className="help__button-submit">Отправить</button>
+		</form>
+	);
 }
 
 export default HelpForm;
--- src/components/Help/index.jsx
+++ src/components/Help/index.jsx
@@ -1,11 +1,13 @@
 import './Help.css';
 
 import HelpList from './HelpList';
+import HelpForm from './HelpForm';
 
 function Help() {
 	return (
 		<div className="help__container">
 			<HelpList />
+			<HelpForm />
 		</div>
 	);
 }
